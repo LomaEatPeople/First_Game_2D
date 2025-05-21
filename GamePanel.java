2a42d4f29cd5cc0ba8f236fbb4ea9d3aa7906ea3
Phatsakon Sonsang <phatsakon.son@dome.tu.ac.th>
2025-05-21 21:19:47 +0700

    set scale gamepanel

--- GamePanel.java
+++ GamePanel.java
@@ -1,7 +1,26 @@
 package main;
 
+import java.awt.Color;
 import javax.swing.JPanel;
+import javax.swing.plaf.DimensionUIResource;
 
 public class GamePanel extends JPanel{
     
+    //SCREEN SETTINGS
+    final int originalTitleSize = 16; // 16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTitleSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixel
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixel
+
+
+    public GamePanel(){
+
+        this.setPreferredSize(new DimensionUIResource(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
 }
--- Main.java
+++ Main.java
@@ -10,6 +10,11 @@ public class Main{
         window.setResizable(false);
         window.setTitle("CS PhatAdventure");
 
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
         window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
