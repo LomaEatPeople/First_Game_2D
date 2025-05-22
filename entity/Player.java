7456807b40d159225ddadc93d39cbf81b96c4e5b
Phatsakon Sonsang <phatsakon.son@dome.tu.ac.th>
2025-05-22 23:24:29 +0700

    Add all file in src on my projact

---
+++ entity/Player.java
@@ -0,0 +1,14 @@
+package entity;
+
+import main.GamePanel;
+import mail.KeyHandler;
+
+public class Player extends Entity{
+    
+    GamePanel gp;
+    KeyHandler keyH;
+
+    public Player(GamePanel gp, KeyHandler keyH){
+        
+    }
+}
